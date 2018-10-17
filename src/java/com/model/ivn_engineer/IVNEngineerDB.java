c2dc931045e041107b78dd60c8691a54ffa36120
KalaiR <kalaimca.gs@gmail.com>
2018-10-17 10:29:29 +0530

    update signals insert in listing page

--- src/java/com/model/ivn_engineer/IVNEngineerDB.java
+++ src/java/com/model/ivn_engineer/IVNEngineerDB.java
@@ -441,10 +441,24 @@ public class IVNEngineerDB {
             Map<String, Object> columns = new HashMap<String, Object>();
             if (rs.next()) {
                 int last_inserted_id = rs.getInt(1);
-//                return last_inserted_id;
+                //Get all the information of signals
+                String sql = "select s.id as sid, s.signal_name as listitem,s.signal_alias as salias,s.signal_description as description,\n"
+                    + "GROUP_CONCAT(DISTINCT(cn.network_name)) as can,\n"
+                    + "GROUP_CONCAT(DISTINCT(ln.network_name)) as lin,\n"
+                    + "GROUP_CONCAT(DISTINCT(hw.network_name)) as hardware from signals as s \n"
+                    + "inner join network as cn on FIND_IN_SET(cn.id,s.can_id_group) > 0 \n"
+                    + "inner join network as ln on FIND_IN_SET(ln.id,s.lin_id_group) > 0 \n"
+                    + "inner join network as hw on FIND_IN_SET(hw.id,s.hw_id_group) > 0 \n"
+                    + "where s.id="+last_inserted_id+" LIMIT 1";
+                ResultSet resultSet = statement.executeQuery(sql);
+                resultSet.last();
                 columns.put("listitem", s.getSignal_name());
                 columns.put("sid", Integer.toString(last_inserted_id));
                 columns.put("description", s.getSignal_description());
+                columns.put("salias", resultSet.getString("salias"));
+                columns.put("can", resultSet.getString("can"));
+                columns.put("lin", resultSet.getString("lin"));
+                columns.put("hardware", resultSet.getString("hardware"));                
                 row.add(columns);
             }
         } catch (Exception e) {
@@ -1037,10 +1051,10 @@ public class IVNEngineerDB {
                     + "GROUP_CONCAT(DISTINCT(cn.network_name)) as can,\n"
                     + "GROUP_CONCAT(DISTINCT(ln.network_name)) as lin,\n"
                     + "GROUP_CONCAT(DISTINCT(hw.network_name)) as hardware from signals as s \n"
-                    + "inner join network as cn on FIND_IN_SET(cn.id,s.can_id_group) > 0 \n"
-                    + "inner join network as ln on FIND_IN_SET(ln.id,s.lin_id_group) > 0 \n"
-                    + "inner join network as hw on FIND_IN_SET(hw.id,s.hw_id_group) > 0 \n"
-                    + "group by s.id";
+                    + "left join network as cn on FIND_IN_SET(cn.id,s.can_id_group) > 0 \n"
+                    + "left join network as ln on FIND_IN_SET(ln.id,s.lin_id_group) > 0 \n"
+                    + "left join network as hw on FIND_IN_SET(hw.id,s.hw_id_group) > 0 \n"
+                    + "group by s.id order by s.id DESC";
             ResultSet resultSet = statement.executeQuery(sql);
             ResultSetMetaData metaData = resultSet.getMetaData();
             int colCount = metaData.getColumnCount();
