319d9a38207162d1406eedfd15123f45f71e391b
Venkatesh <venkatesh@etekchnoservices.com>
2018-10-26 12:59:16 +0530

    ACB Export Function

--- src/java/com/controller/export/FileExportAction.java
+++ src/java/com/controller/export/FileExportAction.java
@@ -6,16 +6,27 @@
 package com.controller.export;
 
 import com.controller.common.JSONConfigure;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.model.acb_owner.ACBOwnerDB;
 import com.model.ivn_engineer.IVNEngineerDB;
+import com.model.ivn_engineer.IVNversion;
+import com.model.ivn_supervisor.Vehicleversion;
 import com.model.ivn_supervisor.VehicleversionDB;
 import com.model.pdb_owner.PDBVersionDB;
+import com.model.pdb_owner.PDBversion;
 import com.opensymphony.xwork2.ActionSupport;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
@@ -43,7 +54,7 @@ public class FileExportAction extends ActionSupport implements ServletRequestAwa
         this.servletRequest = servletRequest;
     }
 
-    public String exportACB(){
+    public String exportACB() {
         //String filePath = servletRequest.getSession().getServletContext().getRealPath("/import");
         File fileToCreate = new File(System.getProperty("user.home") + "/Desktop" + "\\exportACB.csv");
         System.out.println("File Export Path :" + fileToCreate.getAbsolutePath());
@@ -58,27 +69,10 @@ public class FileExportAction extends ActionSupport implements ServletRequestAwa
                     pdb_version = Integer.parseInt(json.get("pdb_version").toString()),
                     ivn_version = Integer.parseInt(json.get("ivn_version").toString());
             
-            Writer writer = new BufferedWriter(new FileWriter(fileToCreate));
-            CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.EXCEL);
-            
-            JSONArray versionDetails = new JSONArray();
-            versionDetails.add("Vehicle Version");
-            versionDetails.add(VehicleversionDB.getVehicleVersionNameFromId(vehicle_version));
-            versionDetails.add("Vehicle");
-            versionDetails.add(VehicleversionDB.getVehicleNameFromId(vehicle_id));
-            versionDetails.add("PDB Version");
-            versionDetails.add(PDBVersionDB.getPDBVersionNameFromId(pdb_version));
-            versionDetails.add("IVN Version");
-            versionDetails.add(IVNEngineerDB.getIVNVersionNameFromId(ivn_version));
-            
-            csvPrinter.printRecord(versionDetails);
-            
-            
-            csvPrinter.flush();
-            writer.close();
-        } catch (IOException ex) {
-            ex.printStackTrace();
+            ExportUtil.exportACBVersionCSV(vehicle_version,vehicle_id,pdb_version,ivn_version,fileToCreate.getAbsolutePath(),parser);
         } catch (ParseException ex) {
+            Logger.getLogger(FileExportAction.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
             Logger.getLogger(FileExportAction.class.getName()).log(Level.SEVERE, null, ex);
         }
 
